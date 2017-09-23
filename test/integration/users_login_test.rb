7a2a7e6d14ae5e6686c7570993d1a0f3aaa0c134
Chima Ezenwanne <ezechima@gmail.com>
2017-09-23 21:09:33 +0100

    Fix subtle bugs

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -55,5 +55,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_url
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+#simulate clicking a logout link a second time.
+    delete logout_path
+    follow_redirect!
+    assert_select "a[href=?]", login_url
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
