990b3dd9b5ed21b3074d3cca4197730d8f6a389b
Chima Ezenwanne <ezechima@gmail.com>
2017-09-24 19:52:22 +0100

    create edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
 end
+#example@railstutorial.org
+#foobar
